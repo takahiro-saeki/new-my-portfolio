0ddc341ad8bc3e5eae4c5aba1abf1d7c26eeb29b
takahiro-saeki <hiroooon69@gmail.com>
2018-04-08 09:19:59 +0900

    updates

--- src/js/lang/en.js
+++ src/js/lang/en.js
@@ -14,7 +14,25 @@ const en = {
     }
   ],
   NAME: 'TAKAHIRO SAEKI',
-  NAME_DESC: 'front end developer'
+  NAME_DESC: 'front end developer',
+  SKILL: {
+    LANGUAGE: {
+      TITLE: 'Language',
+      MAIN_AREA: `
+        I’m front-end developer. Although my speciality is javascript, I can use several language. Especially,
+        I like language that I get involved with front-end development.`,
+      LIST_AREA: 'I can use languages: HTML5, CSS3, javascript, ECMAScript 2015&2016, PHP.'
+    },
+    FRAMEWORK: {
+      TITLE: 'FrameWork',
+      MAIN_AREA: `
+        Nowadays, our front-end development are hypertrophic. In order to improve our environment,
+        javascript framework is so necessary. Especially, I like React.js. React.js is view library.
+        if we implement huge APP, we need flux architecture. My recommend is Redux.js.
+      `,
+      LIST_AREA: 'I can use Frameworks: React.js, react-native, Redux, relay, Vue.js, angular2.'
+    }
+  }
 }
 
 export default en;
--- src/js/lang/ja.js
+++ src/js/lang/ja.js
@@ -14,7 +14,16 @@ const ja = {
     }
   ],
   NAME: '三枝木 貴浩',
-  NAME_DESC: 'フロントエンドエンジニア'
+  NAME_DESC: 'フロントエンドエンジニア',
+  SKILL: {
+    LANGUAGE: {
+      TITLE: 'プログラミング言語',
+      MAIN_AREA: `フロントエンドエンジニア。主にReact.jsを使用した開発を得意とする。
+      universal javascriptやECMAScriptの次期使用に強い関心があります。またVue.jsやangular, cycle.jsといった
+      javascript framework全般に関心があります。`,
+      LIST_AREA: ''
+    }
+  }
 }
 
 export default ja;
