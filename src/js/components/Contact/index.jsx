d981f382b277aa52410e77fe0476c2132599c02a
takahiro-saeki <hiroooon69@gmail.com>
2018-04-10 20:13:55 +0900

    update: updated dependencies.

--- src/js/components/Contact/index.jsx
+++ src/js/components/Contact/index.jsx
@@ -5,21 +5,26 @@ import RaisedButton from 'material-ui/RaisedButton';
 const Contact = () => (
   <div>
     <TextField
-      hintText="Hint Text"
-      floatingLabelText="Fixed Floating Label Text"
-      floatingLabelFixed={true}
-    /><br />
-    <TextField
-      hintText="Hint Text"
-      floatingLabelText="Fixed Floating Label Text"
-      floatingLabelFixed={true}
-    /><br />
-    <TextField
-      hintText="Hint Text"
-      floatingLabelText="Fixed Floating Label Text"
-      floatingLabelFixed={true}
-    /><br />
-    <RaisedButton label="Primary" primary={true} />
+          hintText="yamada tarou"
+          floatingLabelText="name"
+          floatingLabelFixed={true}
+        />
+        <TextField
+          hintText="email"
+          floatingLabelText="xyz-placeholder@gmail.com"
+          floatingLabelFixed={true}
+        />
+        <TextField
+          hintText="subject"
+          floatingLabelText="something which you want to ask me."
+          floatingLabelFixed={true}
+        />
+        <TextField
+          hintText="content"
+          floatingLabelText="here is content"
+          floatingLabelFixed={true}
+        />
+      <RaisedButton label="Primary" primary={true} fullWidth={true} />
   </div>
 )
 
