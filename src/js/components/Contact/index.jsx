d0fae27b5a2590d81503009d1dbac7382f00ad13
takahiro-saeki <hiroooon69@gmail.com>
2018-04-28 23:56:32 +0900

    update: proceeded form feature.

--- src/js/components/Contact/index.jsx
+++ src/js/components/Contact/index.jsx
@@ -1,47 +1,117 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { zipObj, isEmpty } from 'ramda';
+import isEmail from 'validator/lib/isEmail';
 import { Element } from 'react-scroll';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import { Card } from 'material-ui/Card';
 import SectionHeader from 'components/SectionHeader';
-import Container from './style';
+import { Container, BtnContainer } from './style';
 
-const Contact = () => (
-  <Element name="contact">
-    <Container>
-      <SectionHeader title="Contact" />
-      <Card style={{ padding: '1rem' }}>
-        <form
-          action="https://form.run/api/v1/r/jtbMRyJTs0cSTI4phiqC"
-          method="post"
-        >
-          <TextField
-            type="name"
-            hintText="yamada tarou"
-            floatingLabelText="name"
-            floatingLabelFixed
-            fullWidth
-          />
-          <br />
-          <TextField
-            hintText="email"
-            floatingLabelText="xyz-placeholder@gmail.com"
-            floatingLabelFixed
-            fullWidth
-          />
-          <br />
-          <TextField
-            hintText="content"
-            floatingLabelText="here is content"
-            floatingLabelFixed
-            fullWidth
-          />
-          <br />
-          <RaisedButton label="Primary" primary />
-        </form>
-      </Card>
-    </Container>
-  </Element>
-);
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      title: '',
+      content: '',
+      isValid: false
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('data', {prevProps, prevState, currentState: this.state })
+    /*
+    if(!this.state.isValid) {
+      if(!this.validate(this.state)) {
+
+      }
+      this.setState({isValid: true})
+    }
+    */
+  }
+
+  validate = (formData) => {
+    if(isEmpty(formData.name)) {
+      return false
+    }
+    if(!isEmail(formData.email)) {
+      return false
+    }
+    if(isEmpty(formData.title)) {
+      return false
+    }
+    if(isEmpty(formData.content)) {
+      return false
+    }
+    return true
+  }
+
+  handleChange = (flag, text) => {
+    const zip = zipObj([flag])([text]);
+    this.setState(zip);
+  }
+
+  render() {
+    const { isValid } = this.state;
+
+    return (
+      <Element name="c;;ontact">
+        <Container>
+          <SectionHeader title="Contact" />
+          <Card style={{ padding: '1rem' }}>
+            <form
+              action="https://form.run/api/v1/r/jtbMRyJTs0cSTI4phiqC"
+              method="post"
+            >
+              <TextField
+                type="name"
+                hintText="yamada tarou"
+                floatingLabelText="name"
+                floatingLabelFixed
+                fullWidth
+                onChange={e => this.handleChange('name', e.target.value)}
+              />
+              <br />
+              <TextField
+                hintText="email"
+                floatingLabelText="xyz-placeholder@gmail.com"
+                floatingLabelFixed
+                fullWidth
+                onChange={e => this.handleChange('email', e.target.value)}
+              />
+              <br />
+              <TextField
+                hintText="title"
+                floatingLabelText="here is content"
+                floatingLabelFixed
+                fullWidth
+                onChange={e => this.handleChange('title', e.target.value)}
+              />
+              <br />
+              <TextField
+                hintText="content"
+                floatingLabelText="here is content"
+                floatingLabelFixed
+                fullWidth
+                onChange={e => this.handleChange('content', e.target.value)}
+              />
+              <br />
+            <BtnContainer>
+                <RaisedButton
+                  label="SUBMIT"
+                  primary
+                  fullWidth
+                  disabled={!isValid}
+                />
+            </BtnContainer>
+            </form>
+          </Card>
+        </Container>
+      </Element>
+    )
+  }
+}
 
 export default Contact;
