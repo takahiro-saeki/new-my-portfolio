ffab9f10bdf67baed062e708498ba6a6211e34bd
takahiro-saeki <hiroooon69@gmail.com>
2018-05-04 12:41:44 +0900

    fixed: bug of height.

--- src/js/components/Timeline/ListChild/style.js
+++ src/js/components/Timeline/ListChild/style.js
@@ -16,5 +16,5 @@ export const DescArea = styled.div`
   border-radius: 0.5rem;
   box-shadow: rgba(0, 0, 0, 0.117647) 0px 1px 6px,
     rgba(0, 0, 0, 0.117647) 0px 1px 4px;
-  height: 100%;
+  height: auto;
 `;
