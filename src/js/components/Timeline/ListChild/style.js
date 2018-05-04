e0ce2d9368cc01ddb111a1c32dd7bc94d83b8ba8
takahiro-saeki <hiroooon69@gmail.com>
2018-05-04 20:31:57 +0900

    update: styled change.

--- src/js/components/Timeline/ListChild/style.js
+++ src/js/components/Timeline/ListChild/style.js
@@ -17,5 +17,5 @@ export const DescArea = styled.div`
   border-radius: 0.5rem;
   box-shadow: rgba(0, 0, 0, 0.117647) 0px 1px 6px,
     rgba(0, 0, 0, 0.117647) 0px 1px 4px;
-  height: auto;
+  height: 100%;
 `;
