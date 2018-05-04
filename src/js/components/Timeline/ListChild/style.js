caccee8084d90b509afb729432f637774ba27290
takahiro-saeki <hiroooon69@gmail.com>
2018-05-04 12:39:31 +0900

    update

--- src/js/components/Timeline/ListChild/style.js
+++ src/js/components/Timeline/ListChild/style.js
@@ -6,7 +6,7 @@ export const CompanyArea = styled.div`
   border-radius: 0.5rem;
   box-shadow: rgba(0, 0, 0, 0.117647) 0px 1px 6px,
     rgba(0, 0, 0, 0.117647) 0px 1px 4px;
-  height: 100%;
+  height: auto;
   color: #fff;
 `;
 
